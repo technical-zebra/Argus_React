6a39eab2af4a6129a0b0056a359aff4a7a6fe938
technical-zebra <ke.zhang3@my.jcu.edu.au>
2023-02-22 21:27:59 +0800

    Completed 1st ver header, added bootstrap5 dependency, and added image resources

---
+++ my-app/src/header.js
@@ -0,0 +1,34 @@
+import logo from './Images/logo.png'
+import './style.css'
+
+function ArgusHeader() {
+    return (
+        <header>
+            <div className='d-flex justify-content-between align-items-center'>
+                <div className='header-logo-block'>
+                    <img className='logo' src={logo} alt="argus logo" />
+                </div>
+
+                <nav className='header-nav-block'>
+                    <ul className='d-flex flex-row mp-1 nav-ul'>
+                        <li>
+                            <a><span>Home</span></a>
+                        </li>
+                        <li>
+                            <a><span>Models</span></a>
+                        </li>
+                        <li>
+                            <a><span>Modelling as a Service</span></a>
+                        </li>
+                        <li>
+                            <a><span>Company</span></a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </header>
+    );
+}
+
+export default ArgusHeader;
