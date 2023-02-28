b97fc05e30641d6b702f7867e9319165e77932e4
technical-zebra <ke.zhang3@my.jcu.edu.au>
2023-02-28 16:03:42 +0800

    updated slideshow gallery component

--- my-app/src/gallery.js
+++ my-app/src/gallery.js
@@ -2,46 +2,61 @@ import React, { useState, useEffect } from "react";
 import "./gallery.css";
 
 function Gallery({ title, images }) {
-  const [slideIndex, setSlideIndex] = useState(1);
+  const [slideIndex, setSlideIndex] = useState(0);
+  const [slideLength, setSlideLength] = useState(0)
 
   useEffect(() => {
     showImage(slideIndex);
   }, [slideIndex]);
 
   function showImage(n) {
+
+    console.log("showImage: "+n)
     const slides = document.getElementsByClassName("slides");
     const dots = document.getElementsByClassName("dots");
     let i;
 
-    if (n > slides.length) {
-      setSlideIndex(1);
-    }
-
-    if (n < 1) {
-      setSlideIndex(slides.length);
-    }
+    setSlideLength(slides.length - 1)
+    console.log("length: "+slideLength)
 
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
+      slides[i].style.opacity = 0;
     }
 
-    slides[slideIndex - 1].style.display = "block";
+    slides[n].style.display = "block";
 
     for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
     }
 
-    dots[slideIndex - 1].className += " active";
+    dots[n].className += " active";
 
-    slides[slideIndex].style.opacity = 1;
+    slides[n].style.opacity = 1;
+  }
+
+  function setIndexWithinBoundary(index) {
+    setSlideIndex(() => {
+      if (index > slideLength) {
+        return 0;
+      } else if (index < 0) {
+        return 3;
+      } else {
+        return index;
+      }
+    });
   }
 
   function plusIndex(n) {
-    setSlideIndex(slideIndex + n);
+    setIndexWithinBoundary(slideIndex + n)
+    showImage(slideIndex)
+    console.log(slideIndex)
   }
 
   function currentSlide(n) {
-    setSlideIndex(n);
+    setIndexWithinBoundary(n)
+    console.log(slideIndex)
+    showImage(slideIndex)
   }
 
   return (
@@ -77,7 +92,7 @@ function Gallery({ title, images }) {
           {images.map((item, index) => (
             <span
               className="dots"
-              onClick={() => currentSlide(index + 1)}
+              onClick={() => currentSlide(index)}
             ></span>
           ))}
         </div>
