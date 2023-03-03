9c3c131151351ac80a4c2049a83366ce0319a4a8
technical-zebra <ke.zhang3@my.jcu.edu.au>
2023-03-03 22:42:30 +0800

    Deployed website

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,11 @@ import ArgusHeader from "./header";
 import ArgusFooter from "./footer";
 import Homepage from "./home";
 import Models from "./models";
+import MaaS from "./maas";
+import Company from "./company";
+import Contact from "./contact";
+import PrivacyPolicy from "./privacy_policy";
+import NotFound from "./not_found";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +19,11 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route path="/models" element={<Models />} />
+        <Route path="/maas" element={<MaaS />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/privacy_policy" element={<PrivacyPolicy />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <ArgusFooter />
     </Router>
